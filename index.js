ae761ab11e9a88d7e60fe280c92b4631dae098d8
Anthony Ciccone <anthonyciccone@Anthonys-MacBook-Air.local>
2023-09-03 14:10:23 -0600

    added index.js

---
+++ index.js
@@ -0,0 +1,111 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+// const createLogo = require("./lib/logo");
+const colorNames = require("colors");
+const SVGStart = '<svg xmlns="http://www.w3.org/2000/svg" height="200" width="300">'
+const SVGEnd = '</svg>'
+const Square = require("./lib/square");
+const Triangle = require("./lib/triangle");
+const Circle = require("./lib/circle")
+
+
+function init () {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Enter up to three characters",
+                name: "characters",
+            },
+            {
+                type: "input",
+                message: "Select text color",
+                name: "textColor",
+                validate: function (value) {
+                    const isColorName = colorNames.hasOwnProperty(value.toLowerCase());
+                    const isHexColor = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{8})$/.test(value);
+
+                    if (isColorName || isHexColor) {
+                        return true;
+                    }
+
+                    return "Please enter a valid color name or a valid hexadecimal color code (e.g., blue or #RRGGBB or #RRGGBBAA)";
+                },
+
+            },
+            {
+                type: "list",
+                message: "Select shape",
+                choices: ["circle", "triangle", "square"],
+                name: "shape",
+            },
+            {
+                type: "input",
+                message: "Select shape color",
+                name: "shapeColor",
+                validate: function (value) {
+                    const isColorName = colorNames.hasOwnProperty(value.toLowerCase());
+                    const isHexColor = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{8})$/.test(value);
+
+                    if (isColorName || isHexColor) {
+                        return true;
+                    }
+
+                    return "Please enter a valid color name or a valid hexadecimal color code (e.g., blue or #RRGGBB or #RRGGBBAA)";
+                },
+            },
+        ])
+    
+        .then((data) => {
+            // const logo = createLogo(data);
+            // SVGFile(logo);
+            if (data.shape === "square") {
+                const square = new Square(data.shapeColor, data.characters, data.textColor);
+                const SVGString = SVGStart + square.render() + SVGEnd
+                fs.writeFile("logo.svg", SVGString, (err) => {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.log("Generated logo.svg");
+                    }
+                });
+            }
+                if (data.shape === "circle") {
+                    const circle = new Circle(data.shapeColor, data.characters, data.textColor);
+                    const SVGString = SVGStart + circle.render() + SVGEnd
+                    fs.writeFile("logo.svg", SVGString, (err) => {
+                        if (err) {
+                            console.error(err);
+                        } else {
+                            console.log("Generated logo.svg");
+                        }
+                    });
+                }
+                else if (data.shape === "triangle") {
+                    const triangle = new Triangle(data.shapeColor, data.characters, data.textColor);
+                    const SVGString = SVGStart + triangle.render() + SVGEnd
+                    fs.writeFile("logo.svg", SVGString, (err) => {
+                        if (err) {
+                            console.error(err);
+                        } else {
+                            console.log("Generated logo.svg");
+                        }
+                    });
+                }
+            })
+    .catch((error) => {
+            console.error(error);
+        });
+    }
+
+function SVGFile(content) {
+        fs.writeFile("logo.svg", content, (err) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log("Generated logo.svg");
+            }
+        });
+    };
+
+init();
